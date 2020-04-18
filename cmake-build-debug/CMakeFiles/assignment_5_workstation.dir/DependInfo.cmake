125c1ce40603040ac6a4c5fb4c056d0a303a8716
firebearrex <rex.cheng@qq.com>
2020-04-17 17:11:44 -0700

    Starting codes

---
+++ cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/file_util.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/file_util.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/http_methods.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/http_methods.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/http_request.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/http_request.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/http_server.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/http_server.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/http_util.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/http_util.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/media_util.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/media_util.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/network_util.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/network_util.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/properties.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/properties.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/string_util.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/string_util.c.o"
+  "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/src/time_util.c" "/Users/rex/Documents/GitHub/firebearrex private/assignment-5-workstation/cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/src/time_util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../assignment-5-threadpool-workstation/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/assignment_5_workstation.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment_5_workstation.dir/src/file_util.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/http_methods.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/http_request.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/http_server.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/http_util.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/media_util.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/network_util.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/properties.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/string_util.c.o"
+  "CMakeFiles/assignment_5_workstation.dir/src/time_util.c.o"
+  "assignment_5_workstation"
+  "assignment_5_workstation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assignment_5_workstation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
